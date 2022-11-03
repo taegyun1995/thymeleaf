f42dea52c45a05e6d58cda227fc3c6e0ac0e1eb6
gyunny <xorbs15923@naver.com>
2022-11-03 14:47:35 +0900

    Thymeleaf basic

--- src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
+++ src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThymeleafBasicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThymeleafBasicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafBasicApplication.class, args);
+    }
 
 }
